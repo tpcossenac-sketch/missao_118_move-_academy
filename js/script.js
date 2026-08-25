9bdb4690bd50c9e443d75fbd067a23a10030c98d
tpcossenac-sketch <tpcossenac@gmail.com>
2026-08-25 11:14:04 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,40 +6,79 @@ window.addEventListener("DOMContentLoaded", function () {
     const menuUnidades = document.getElementById("menuUnidades");
     const menuTreinos = document.getElementById("menuTreinos");
 
-    if (!menuPlanos || !menuUnidades || !menuTreinos) return;
+    if (menuPlanos && menuUnidades && menuTreinos) {
 
-    if (aluno === "true") {
+        if (aluno === "true") {
 
-        menuPlanos.style.display = "none";
-        menuUnidades.style.display = "none";
-        menuTreinos.style.display = "block";
+            menuPlanos.style.display = "none";
+            menuUnidades.style.display = "none";
+            menuTreinos.style.display = "block";
 
-    } else {
+        } else {
 
-        menuPlanos.style.display = "block";
-        menuUnidades.style.display = "block";
-        menuTreinos.style.display = "none";
+            menuPlanos.style.display = "block";
+            menuUnidades.style.display = "block";
+            menuTreinos.style.display = "none";
+
+        }
+
+    }
+
+
+    // MENU MOBILE
+
+    const menuMobile = document.getElementById("menu-mobile");
+    const menu = document.getElementById("menu");
+
+    if (menuMobile && menu) {
+
+        // Clique com mouse
+        menuMobile.addEventListener("click", function () {
+            menu.classList.toggle("ativo");
+        });
+
+
+        // EVENTO DE TOQUE 1
+        menuMobile.addEventListener("touchstart", function () {
+            menuMobile.style.transform = "scale(0.9)";
+        });
+
+
+        // EVENTO DE TOQUE 2
+        menuMobile.addEventListener("touchend", function () {
+
+            menuMobile.style.transform = "scale(1)";
+            menu.classList.toggle("ativo");
+
+        });
 
     }
 
 });
 
-const menuMobile = document.getElementById("menu-mobile");
-const menu = document.getElementById("menu");
 
-menuMobile.addEventListener("click", () => {
-    menu.classList.toggle("ativo");
-});
+// SERVICE WORKER
 
 if ("serviceWorker" in navigator) {
-    window.addEventListener("load", () => {
+
+    window.addEventListener("load", function () {
+
         navigator.serviceWorker
             .register("./sw.js")
-            .then(() => {
+            .then(function () {
+
                 console.log("Service Worker registrado com sucesso!");
+
             })
-            .catch(erro => {
-                console.error("Erro ao registrar o Service Worker:", erro);
+            .catch(function (erro) {
+
+                console.error(
+                    "Erro ao registrar o Service Worker:",
+                    erro
+                );
+
             });
+
     });
+
 }
